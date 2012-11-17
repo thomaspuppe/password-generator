3b38dc0842751b6bfc3eaab960009af1a64cceb7
Thomas Puppe <info@thomaspuppe.de>
2012-11-17 02:47:05 +0100

    implemented core functionality: password generation

--- index.php
+++ index.php
@@ -1,19 +1,48 @@
 <?php
-require("toro.php");
+require("Toro.php");
+require("PasswordGenerator.php");
 
-class PasswordIndexHandler {
-    function get() {
-      echo "Hello, world";
-    }
-}
+define('DEFAULT_LENGTH', 16);
+define('NUMBER_OF_PASSWORDS', 50);
 
-class PasswordNumberHandler {
-    function get($numberOfCharacters) {
-      echo "Hello, world: " . $numberOfCharacters;
+class PasswordHandler {
+    function get($numberOfCharacters=null) {
+	
+		if ($numberOfCharacters==null) {
+			$numberOfCharacters = DEFAULT_LENGTH;
+		}
+	
+		$passwordGenerator = new PasswordGenerator($numberOfCharacters);
+		
+		echo '<div style="min-width:200px;float:left;">';
+		$passwords = $passwordGenerator->getArrayOfComplicatedPasswords(NUMBER_OF_PASSWORDS);
+		foreach($passwords as $password) {
+			echo $password . '<br>';
+		}
+		echo '</div>';
+		
+		echo '<div style="min-width:200px;float:left;">';
+		$passwords = $passwordGenerator->getArrayOfAlphanumericalPasswords(NUMBER_OF_PASSWORDS);
+		foreach($passwords as $password) {
+			echo $password . '<br>';
+		}
+		echo '</div>';
+		
+		echo '<div style="min-width:200px;float:left;">';
+		$passwords = $passwordGenerator->getArrayOfMnemonicPasswords(NUMBER_OF_PASSWORDS);
+		foreach($passwords as $password) {
+			echo $password . '<br>';
+		}
+		echo '</div>';
+		
+		
+		
+		
     }
 }
 
 Toro::serve(array(
-    "/" => "PasswordIndexHandler",
-	"/:number/" => "PasswordNumberHandler"
+    "/" => "PasswordHandler",
+	"/:number/" => "PasswordHandler",
+	"/:number" => "PasswordHandler"
 ));
